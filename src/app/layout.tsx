08876dabeeff4596c39633d88f458492a69be528
zhenhuamo <zhenhuamo@vip.qq.com>
2025-10-29 21:24:14 +0800

    master

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import ThemeProviderClient from "./components/ThemeProviderClient";
 
@@ -15,6 +16,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-EJC13GF5B2"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-gtag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-EJC13GF5B2');
+          `}
+        </Script>
         <ThemeProviderClient>
           {children}
         </ThemeProviderClient>
